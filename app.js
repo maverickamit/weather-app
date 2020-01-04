db0228ec36f014bcf35f7445ed0ffb2780533703
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-01-04 12:43:33 +0530

    implemented MapBox API

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const request = require("request");
 const url =
   "https://api.darksky.net/forecast/16c76dbed11f350b6b9a6946681648b9/22.5726,88.3639?units=si";
+const geocodeUrl =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibXltYXAyNSIsImEiOiJjazR6Nml0bnYwN2NoM21xdmJhdGh3dm9sIn0.S1sbjP9fdP3Ui7cWGYBitw&limit=1";
+request({ url: geocodeUrl, json: true }, (error, response) => {
+  const geoData = response.body.features[0];
+  console.log(
+    `${geoData.place_name} \nLongitude ${geoData.center[0]} \nLatitude ${geoData.center[1]}`
+  );
+});
 
 request({ url: url, json: true }, (error, response) => {
   const data = response.body;
