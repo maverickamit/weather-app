04b388d71c61c975c3d64e0deefaacfdd05bea90
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-01-04 12:24:16 +0530

    using darksky api

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const request = require("request");
 const url =
-  "https://api.darksky.net/forecast/16c76dbed11f350b6b9a6946681648b9/37.8267,-122.4233";
+  "https://api.darksky.net/forecast/16c76dbed11f350b6b9a6946681648b9/22.5726,88.3639?units=si";
+
+request({ url: url, json: true }, (error, response) => {
+  const data = response.body;
+  console.log(
+    `It is currently ${Math.round(
+      data.currently.temperature
+    )} degrees outside. There is ${Math.round(
+      data.currently.precipProbability * 100
+    )}% chance of rain.`
+  );
+});
